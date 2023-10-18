ce9875fa881e40e7d48152410393b35ac1ca6172
elice77 <elice77@elice77ui-iMac.local>
2023-10-18 18:36:44 +0900

    Rename: test -> boards 라우터 파일명 변경

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
-const test = require("./routes/test");
+const boardRouter = require("./routes/boards");
 
-app.use("/api", test);
+app.use("/api/boards", boardRouter);
 
 const port = 3002; //node 서버가 사용할 포트 번호, 리액트의 포트번호(3000)와 충돌하지 않게 다른 번호로 할당
 app.listen(port, () => {
