87c6e2e5262a017ab5832b0016906754548b16fb
kcjfgnl9205 <kcjfgnl9205@gmail.com>
2023-10-18 23:05:13 +0900

    feat: CORS미들웨어 추가

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const boardRouter = require("./routes/boards");
+
+app.use(cors());
 
 app.use("/api/boards", boardRouter);
 
