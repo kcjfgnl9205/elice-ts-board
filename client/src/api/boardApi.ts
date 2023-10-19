2f6e01611b95beacb614c23fc691b4972d4d905c
Dev-koo <gyu523@naver.com>
2023-10-20 00:39:05 +0900

    feat: BoardList 에러처리

--- client/src/api/boardApi.ts
+++ client/src/api/boardApi.ts
@@ -2,6 +2,12 @@ import axios from "axios";
 
 // Get JSON file from mock folder using axios.
 export const getBoardList = async () => {
-  const response = await axios.get("/src/mock/boards.json");
-  return response.data.boards;
+  try {
+    const response = await axios.get("/src/mock/boards.json");
+    return response.data.boards;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw new Error(error.message);
+    }
+  }
 };
