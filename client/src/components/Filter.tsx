a6c7ded99694a634768c5e7ac54380c5257a0687
Dev-koo <gyu523@naver.com>
2023-10-23 01:30:47 +0900

    fix: filter에서 selected 속성 삭제

--- client/src/components/Filter.tsx
+++ client/src/components/Filter.tsx
@@ -4,10 +4,9 @@ const Filter = () => {
       <select
         id="filter"
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        defaultValue={"latest"}
       >
-        <option selected value="latest">
-          최신순
-        </option>
+        <option value="latest">최신순</option>
         <option value="oldest">오래된 순</option>
         <option value="commentCount">댓글 순</option>
         <option value="recommended">추천 순</option>
